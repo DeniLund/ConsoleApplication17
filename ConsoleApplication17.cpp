ede64fb408ac8a733a0a979289d2ea22d3992f08
Denis Lund <vasilenko9999@gmail.com>
2021-12-23 16:40:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication17.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <locale>
+#include <cmath>
+
+class Vector
+{
+public:
+    Vector()
+    {}
+    Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z)   
+    {
+        double dVectorLength(0.0);
+        dVectorLength = sqrt(x * x + y * y + z * z);
+    }
+    void Show()
+    {
+        setlocale(LC_CTYPE, "rus");
+        std::cout << '\n' << x << ' ' << y << ' ' << z;
+        std::cout << "\n Длина вектора: " << sqrt(x * x + y * y + z * z) << " \n\n\a";
+    }
+    
+private:
+    double x = 110;
+    double y = 90;
+    double z = 1;
+};
+
+int main()
+{
+    Vector v;
+    v.Show();
+}
